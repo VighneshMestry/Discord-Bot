122c0a9d6b4e6a52fac3a9fd1bcd5e0aae0a95f2
Vighnesh Mestry <mestryvighnesh27@gmail.com>
2024-11-18 19:25:54 +0530

    Bot created

---
+++ index.js
@@ -0,0 +1,29 @@
+import { Client, GatewayIntentBits } from "discord.js";
+import 'dotenv/config'
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  message.reply({
+    content: "Hello from bot",
+  });
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+  
+    if (interaction.commandName === 'ping') {
+      await interaction.reply('Pong!');
+    }
+  });
+
+client.login(
+  process.env.TOKEN
+);
